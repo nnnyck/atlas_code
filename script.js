51d69e3a7f86c4da9ca18bf8f0574c4b75b75542
nyckolas <nycparroseixas@gmail.com>
2024-04-10 19:51:07 -0300

    reformulação da tela

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
-function showContent(content) {
-    // Oculta todos os conteúdos
-    document.getElementById("htmlContent").style.display = "none";
-    document.getElementById("cssContent").style.display = "none";
-    document.getElementById("jsContent").style.display = "none";
-    document.getElementById("phpContent").style.display = "none";
-    
-    // Mostra o conteúdo correspondente ao botão clicado
-    document.getElementById(content + "Content").style.display = "block";
-}
+document.addEventListener("DOMContentLoaded", function() {
+    const container = document.querySelector(".container");
+  
+    // Gerar 100 esferas em posições aleatórias dentro do banner
+    for (let i = 0; i < 100; i++) {
+      const esfera = document.createElement("div");
+      esfera.classList.add("esfera");
+      esfera.style.top = Math.random() * 50 + "vh"; // Posição vertical aleatória dentro do banner
+      esfera.style.left = Math.random() * 100 + "%"; // Posição horizontal aleatória dentro do banner
+      container.appendChild(esfera);
+    }
+  });
+
+  function showSidebar(){
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'flex'
+  }
+
+  function hideSidebar(){
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'none'
+  }
